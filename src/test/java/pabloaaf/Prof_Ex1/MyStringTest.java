edb1023623fa0f7390f005762c005c7619761f97
palvaf02 <palvaf02@estudiantes.unileon.es>
2018-11-20 16:48:27 +0100

    //caminos

--- src/test/java/pabloaaf/Prof_Ex1/MyStringTest.java
+++ src/test/java/pabloaaf/Prof_Ex1/MyStringTest.java
@@ -10,20 +10,20 @@ public class MyStringTest {
 	public void test_equals(){
 		MyString a = new MyString("algo");
 		MyString b = new MyString("algo");
-		assertTrue(a.compareTo(b) == 0);
+		assertTrue(a.compareTo(b) == 0); //1-(2-3)-2-5-7
 	}
 	
 	@Test
 	public void test_NotEquals_sameLength(){
 		MyString a = new MyString("algo");
 		MyString b = new MyString("alco");
-		assertTrue(a.compareTo(b) == 4);
+		assertTrue(a.compareTo(b) == 4); //1-(2-3)-4-7
 	}
 	
 	@Test
 	public void test_NotSameLength(){
 		MyString a = new MyString("algo");
 		MyString b = new MyString();
-		assertTrue(a.compareTo(b) == 4);
+		assertTrue(a.compareTo(b) == 4); //1-6-7
 	}
 }
