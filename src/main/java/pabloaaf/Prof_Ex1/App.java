c64cbc1009fd1a55ab76b48ac6cf533395ecbb65
palvaf02 <palvaf02@estudiantes.unileon.es>
2018-11-20 16:07:37 +0100

    MyString class and method compareTo

---
+++ src/main/java/pabloaaf/Prof_Ex1/App.java
@@ -0,0 +1,13 @@
+package pabloaaf.Prof_Ex1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/pabloaaf/Prof_Ex1/MyString.java
@@ -0,0 +1,23 @@
+package pabloaaf.Prof_Ex1;
+
+public class MyString {
+	private String str;
+	
+	public MyString() {
+		this.str = "";
+	}
+	
+	public int compareTo(MyString segundo) {
+		if(this.str.length() == segundo.str.length()) {
+			char[] chars_str1 = this.str.toCharArray();
+			char[] chars_str2 = segundo.str.toCharArray();
+			
+			for(int i=0; i< chars_str1.length; i++) {
+				if(chars_str1[i] != chars_str2[i]) {
+					return chars_str1[i] - chars_str2[i];
+				}
+			}
+		}		
+		return this.str.length() - segundo.str.length();
+	}
+}
